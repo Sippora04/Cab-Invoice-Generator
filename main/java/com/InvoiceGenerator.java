e55ce3c345192fb396eea36076791e74c8299d16
Sippora04 <sippora.toppo95@gmail.com>
2020-11-17 16:40:20 +0530

    Added: multiple rides, and calculating the aggregate total for all

--- main/java/com/InvoiceGenerator.java
+++ main/java/com/InvoiceGenerator.java
@@ -7,8 +7,15 @@ public class InvoiceGenerator {
 	private static final double MINIMUM_FARE = 5.0;
 
 	public double calculateFare(double distance, int time) {
-		// TODO Auto-generated method stub
 		double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
 		return Math.max(totalFare, MINIMUM_FARE);
+	}
+
+	public double calculateFare(Ride[] rides) {
+		double totalFare = 0.0;
+		for (Ride ride : rides) {
+			totalFare += this.calculateFare(ride.distance, ride.time);
+		}
+		return totalFare;
 	}
 }
