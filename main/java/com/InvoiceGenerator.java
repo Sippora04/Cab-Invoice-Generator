ad2332e17529ada883cbb54830c98859bb2f77a6
Sippora04 <sippora.toppo95@gmail.com>
2020-11-17 16:31:00 +0530

    Added: calculating fare

---
+++ main/java/com/InvoiceGenerator.java
@@ -0,0 +1,14 @@
+package com;
+
+public class InvoiceGenerator {
+
+	private static final double MINIMUM_COST_PER_KM = 10.0;
+	private static final int COST_PER_TIME = 1;
+	private static final double MINIMUM_FARE = 5.0;
+
+	public double calculateFare(double distance, int time) {
+		// TODO Auto-generated method stub
+		double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
+		return Math.max(totalFare, MINIMUM_FARE);
+	}
+}
